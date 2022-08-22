6867663e37c752c3bd9f8174711fdd284ad78222
bmatlik <106585052+bmatlik@users.noreply.github.com>
2022-08-22 17:30:03 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -11,5 +11,9 @@ echo "<br> Email: $email";
 $age = filter_var($_POST['age'], FILTER_SANITIZE_NUMBER_INT);
 $name = htmlspecialchars($_POST['name'], ENT_QUOTES);
 $role = htmlspecialchars($_POST['role'], ENT_QUOTES);
+$favorite_subject = htmlspecialchars($_POST['favorite_subject'], ENT_QUOTES);
+$favorite_color = htmlspecialchars($_POST['favorite_color'], ENT_QUOTES);
+$favorite_os = htmlspecialchars($_POST['favorite_os'], ENT_QUOTES);
+$favorite_phone = htmlspecialchars($_POST['favorite_phone'], ENT_QUOTES);
 }
 ?>
