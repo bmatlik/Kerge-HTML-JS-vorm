0ffc6e0a487d8a77c41a11035e181a47ebf20477
bmatlik <106585052+bmatlik@users.noreply.github.com>
2022-08-22 16:02:32 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,15 @@
+<!DOCTYPE HTML>
 <?php
+if (!isset($_POST['submit'])){
+    echo "You have not submitted the form!";
+    exit();
+}
 if (isset($_POST['submit'])){
 echo "You have submitted the form succesfully!";
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+echo "<br> Email: $email";
+$age = filter_var($_POST['age'], FILTER_SANITIZE_NUMBER_INT);
+$name = htmlspecialchars($_POST['name'], ENT_QUOTES);
+$role = htmlspecialchars($_POST['role'], ENT_QUOTES);
 }
-else {
-    echo "You have not submitted the form.";
-}
-?> 
+?>
